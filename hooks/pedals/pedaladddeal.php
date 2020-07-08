09796006ed65901536085d82b38b47d7e4b6ab84
Miros <lya-job@yandex.ru>
2020-07-08 09:59:11 +0200

    pedal import with basket

--- hooks/pedals/pedaladddeal.php
+++ hooks/pedals/pedaladddeal.php
@@ -27,29 +27,41 @@ $zip = '777';
 $fund = 120;
 // стадия сделки
 $stage = 'C10:NEW';
+// перечисляем соответственно количеству товаров в корзине - PRODUCT_NAME (марка модель двигатель), количество, цена - за количество
+$productarr = array(
+    array('PRODUCT_ID' => 11556,
+        'PRODUCT_NAME' => 'AUDI(FAW) A6L(488) 40TFSI',
+        'QUANTITY' => 1,
+        'PRICE' => 300),
+    // 'PRODUCT_ID' всегда => 11556!
+    array('PRODUCT_ID' => 11556,
+        'PRODUCT_NAME' => 'CHRYSLER PT CRUISER(PT_) 1.6',
+        'QUANTITY' => 2,
+        'PRICE' => 300)
+);
 
 // проверяем есть ли уже контакт с таким емейлом
 $appParams = http_build_query(array(
-        'halt' => 0,
-        'cmd' => array(
-            'get_contact' => 'crm.contact.list?'
-                .http_build_query(array(
-                    'order' => array('ID' => "ASC"),
-                    'filter' => array('EMAIL'=>$email),
-                    'select' => array("*","UF_*", "PHONE", "EMAIL")
-                ))
-        )
+    'halt' => 0,
+    'cmd' => array(
+        'get_contact' => 'crm.contact.list?'
+            .http_build_query(array(
+                'order' => array('ID' => "ASC"),
+                'filter' => array('EMAIL'=>$email),
+                'select' => array("*","UF_*", "PHONE", "EMAIL")
+            ))
+    )
 ));
 // скрипт для передачи данных в CRM - можете потом упаковать его в одну функцию
 $appRequestUrl = 'https://'.$domain.'/rest/'.$user.'/'.$auth.'/batch';
 $curl=curl_init();
 curl_setopt_array($curl, array(
-        CURLOPT_SSL_VERIFYPEER => 0,
-        CURLOPT_POST => 1,
-        CURLOPT_HEADER => 0,
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $appRequestUrl,
-        CURLOPT_POSTFIELDS => $appParams
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_POST => 1,
+    CURLOPT_HEADER => 0,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $appRequestUrl,
+    CURLOPT_POSTFIELDS => $appParams
 ));
 $out=curl_exec($curl);
 $out = json_decode($out, 1);
@@ -121,52 +133,71 @@ if($cid) {
 }
 // имеем готовый контакт и добавляем сделку
 if($cid) {
-       $appParams = http_build_query(array(
-            'halt' => 0,
-            'cmd' => array(
-                'make_deal' => 'crm.deal.add?'
-                    .http_build_query(array(
-                            'fields' => array('TYPE_ID' => 'SALE',
-                                'TITLE' =>  'New order from:'.$name.' '.$lastname,
-                                'CONTACT_ID' => $cid,
-                                'TYPE_ID' => 'SALE',
-                                'ASSIGNED_BY_ID' => 2240,
-                                //'SOURCE_ID' => 'WEB',
-                                'CATEGORY_ID' => 10,
-                                'STAGE_ID' => 'C10:NEW',
-                                'OPPORTUNITY' => $fund,
-                                'UF_CRM_1594121498' => $name.' '.$lastname,
-                                'UF_CRM_1594121513' => $street,
-                                'UF_CRM_1594121528' => $country.' '.$zip.' '.$state,
-                                'UF_CRM_1594121544' => $delmethod,
-                                'UF_CRM_1587361552' => 1,
-                                'UF_CRM_1587361577' => 1
+    $appParams = http_build_query(array(
+        'halt' => 0,
+        'cmd' => array(
+            'make_deal' => 'crm.deal.add?'
+                .http_build_query(array(
+                        'fields' => array('TYPE_ID' => 'SALE',
+                            'TITLE' =>  'New order from:'.$name.' '.$lastname,
+                            'CONTACT_ID' => $cid,
+                            'TYPE_ID' => 'SALE',
+                            'ASSIGNED_BY_ID' => 2240,
+                            //'SOURCE_ID' => 'WEB',
+                            'CATEGORY_ID' => 10,
+                            'STAGE_ID' => 'C10:NEW',
+                            'OPPORTUNITY' => $fund,
+                            'UF_CRM_1594121498' => $name.' '.$lastname,
+                            'UF_CRM_1594121513' => $street,
+                            'UF_CRM_1594121528' => $country.' '.$zip.' '.$state,
+                            'UF_CRM_1594121544' => $delmethod,
+                            'UF_CRM_1587361552' => 1,
+                            'UF_CRM_1587361577' => 1
 
-                            )
                         )
                     )
-            )));
-        $appRequestUrl = 'https://'.$domain.'/rest/'.$user.'/'.$auth.'/batch';
-        $curl=curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_SSL_VERIFYPEER => 0,
-            CURLOPT_POST => 1,
-            CURLOPT_HEADER => 0,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $appRequestUrl,
-            CURLOPT_POSTFIELDS => $appParams
-        ));
-        $out=curl_exec($curl);
-        $out = json_decode($out, 1);
-        echo "<pre>";
-        print_r($out);
-        echo "</pre>";
-        // тут по желанию пишешь лог с id сделки, указав корректный путь на сервере
-        $title = 'newordadd';
-        $log = "\n------------------------\n";
-        $log .= date("Y.m.d G:i:s") . "\n";
-        $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
-        $log .= print_r($out['result']['result']['make_deal'], 1);
-        $log .= "\n------------------------\n";
-        //file_put_contents('/loggappnew.log', $log, FILE_APPEND);
+                )
+        )));
+    $appRequestUrl = 'https://'.$domain.'/rest/'.$user.'/'.$auth.'/batch';
+    $curl=curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $appRequestUrl,
+        CURLOPT_POSTFIELDS => $appParams
+    ));
+    $out=curl_exec($curl);
+    $out = json_decode($out, 1);
+    $madeddeal = $out['result']['result']['make_deal'];
+    $appParams = http_build_query(array(
+        'halt' => 0,
+        'cmd' => array(
+            'get_contact' => 'crm.deal.productrows.set?'
+                .http_build_query(array(
+                    'id' => $madeddeal,
+                    'rows' => $productarr
+
+                ))
+        )
+    ));
+    $appRequestUrl = 'https://'.$domain.'/rest/'.$user.'/'.$auth.'/batch';
+    $curl=curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $appRequestUrl,
+        CURLOPT_POSTFIELDS => $appParams
+    ));
+    $out=curl_exec($curl);
+    $out = json_decode($out, 1);
+    $cid = $out['result']['result']['get_contact'][0]['ID'];
+
+    echo "<pre>";
+    print_r($out);
+    echo "</pre>";
+
 }
